f4507ab1325698d1dc93a210ea37f99f610b9f3d
Nathan <nathan@nathan-s.com>
2018-09-25 02:42:14 -0700

    Lexer generator from changelist.

--- src/Lexer/Lexer.py
+++ src/Lexer/Lexer.py
@@ -2,7 +2,50 @@ from . import *
 
 
 class Lexer(object):
+
+    @classmethod
+    def lex_state_mapper(cls, ch: str, alpha_state: int, digit_state: int, operator_state: int, undefined_state: int):
+        """helper function for different state mappings"""
+        if ch.isalpha():
+            return alpha_state
+        elif ch.isdigit():
+            return digit_state
+        elif TokenOperator.is_symbol(ch):
+            return operator_state
+        else:
+            return undefined_state
+
+    @classmethod
+    def lex_recursive_generator(cls, file_ptr, current_state=1, token_str=''):
+        """yields all token instances given a file pointer"""
+        next_char: str = file_ptr.read(1)
+        if TokenBase.is_reserved(next_char) and TokenBase.is_in_accepting_states(current_state):
+            yield TokenBase.get_token(current_state, token_str)
+            current_state = 1
+            token_str = ''
+        elif TokenBase.is_reserved(token_str) and not TokenBase.is_symbol(token_str):
+            yield TokenBase.get_token(current_state, token_str)
+            current_state = 1
+            token_str = ''
+        else:
+            pass
+        if next_char == '':  # eof
+            return
+        next_state = current_state
+        if not TokenBase.is_symbol(next_char):  # skips whitespace, tabs, newline
+            token_str += next_char
+            if current_state == 1:
+                next_state = cls.lex_state_mapper(next_char, 2, 3, 4, 5)
+            elif current_state == 2:
+                next_state = cls.lex_state_mapper(next_char, 2, 5, 5, 5)
+            elif current_state == 3:
+                next_state = cls.lex_state_mapper(next_char, 5, 3, 5, 5)
+            elif current_state == 4:
+                next_state = cls.lex_state_mapper(next_char, 5, 5, 5, 5)
+            else:
+                next_state = cls.lex_state_mapper(next_char, 5, 5, 5, 5)
+        yield from cls.lex_recursive_generator(file_ptr, next_state, token_str)
+
     @classmethod
     def lexer(cls, file_ptr):
-        return
-        yield
+        yield from cls.lex_recursive_generator(file_ptr)
